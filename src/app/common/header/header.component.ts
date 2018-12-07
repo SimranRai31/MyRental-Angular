9415630f6362e13f454b2eb631fc186750989b9f
simran rai <simranrai314@gmail.com>
2018-12-06 19:16:53 -0500

    Header Component added

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "rental-header",
+    // tslint:disable-next-line:quotemark
+    templateUrl: "./header.component.html",
+    // tslint:disable-next-line:quotemark
+    styleUrls: ["./header.component.scss"]
+})
+
+export class HeaderComponent {
+
+}
